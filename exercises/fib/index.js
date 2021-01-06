ba29d6aca8d555d30b58c7f215bb68bbc652ade8
Ryan Pfister <puhfister@gmail.com>
2021-01-06 11:07:56 -0500

    fibbonacci

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,58 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// linear runtime
+// function fib(n) {
+//   const result = [0, 1];
+//   for (let index = result.length; index <= n; index++) {
+//     result.push(result[index - 1] + result[index - 2]);
+//   }
+
+//   return result[n];
+// }
+
+//exponential time (look at time taken in tests)
+//can be improved with MEMOIZATION (save the result of each fib(arg) call)
+function slow_fib(n) {
+  if (n < 2) {
+    return n;
+  }
+
+  return fib(n - 1) + fib(n - 2);
+}
+
+//mine
+const memo = {};
+
+function ryan_fib(n) {
+  if (memo[n] !== undefined) {
+    return memo[n];
+  }
+
+  if (n < 2) {
+    memo[n] = n;
+    return n;
+  }
+
+  memo[n - 1] = fib(n - 1);
+  memo[n - 2] = fib(n - 2);
+
+  return memo[n - 1] + memo[n - 2];
+}
+
+//Stephen's
+function memoize(fn) {
+  const cache = {};
+  return function (...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    cache[args] = fn(...args);
+    return cache[args];
+  };
+}
+
+const fib = memoize(slow_fib);
 
 module.exports = fib;

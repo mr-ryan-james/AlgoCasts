dd191313c8affa7c456d5b1eb8bd7a9e9a1ae71f
Ryan Pfister <puhfister@gmail.com>
2021-01-08 09:31:08 -0500

    fizzy

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,37 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+// Mine
+function fizzBuzz_mine(n) {
+  for (let index = 1; index <= n; index++) {
+    if (index % 3 === 0 && index % 5 === 0) {
+      console.log("fizzbuzz");
+    }
+    if (index % 3 !== 0 && index % 5 !== 0) {
+      console.log(index);
+    } else if (index % 3 === 0) {
+      debugger;
+      console.log("fizz");
+    } else if (index % 5 === 0) {
+      debugger;
+      console.log("buzz");
+    }
+  }
+}
+
+//Stephen's
+function fizzBuzz(n) {
+  for (let i = 1; i <= n; i++) {
+    if (i % 15 === 0) {
+      console.log("fizzbuzz");
+    } else if (i % 3 === 0) {
+      console.log("fizz");
+    } else if (i % 5 === 0) {
+      console.log("buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
 
 module.exports = fizzBuzz;

7f63cbe0605dc678af50da3be5a7d57f9a81ee2b
Ryan Pfister <puhfister@gmail.com>
2021-01-09 09:03:32 -0500

    doing that sweet sweet algorithms course

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,96 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function buildCharacterMap(str) {
+  const chars = {};
+  for (const char of str) {
+    chars[char] = char[char] + 1 || 1;
+  }
+  return chars;
+}
+
+const noPunctuation = /\w/g;
+
+// Mine
+function anagrams_ryan(stringA, stringB) {
+  const strAChars = buildCharacterMap(
+    stringA.match(noPunctuation).join("").toLowerCase()
+  );
+  const strBChars = buildCharacterMap(
+    stringB.match(noPunctuation).join("").toLowerCase()
+  );
+
+  const aKeys = Object.keys(strAChars);
+  const bKeys = Object.keys(strBChars);
+
+  if (aKeys.length !== bKeys.length) {
+    return false;
+  }
+
+  return aKeys.every((akey) => strAChars[akey] === strBChars[akey]);
+}
+
+// Stephen's
+// first solution
+function buildCharMap(str) {
+  const charMap = {};
+
+  for (const char of str.replace(/[^\w]/g, "").toLowerCase()) {
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+
+  return charMap;
+}
+
+function anagrams_first(stringA, stringB) {
+  const aCharMap = buildCharMap(stringA);
+  const bCharMap = buildCharMap(stringB);
+
+  if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+    return false;
+  }
+
+  for (const char in aCharMap) {
+    if (aCharMap[char] !== bCharMap[char]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function sortMethod(a, b) {
+  return a.toLowerCase().localeCompare(b.toLowerCase());
+}
+
+// My second solution using numbers.sort()
+function anagrams_ryan_sort(stringA, stringB) {
+  //I could also use .replace, and keep it as a string
+  //(this method changes it to an array, which drives future decisions)
+  const cleanA = stringA.match(/\w/g).sort(sortMethod);
+  const cleanB = stringB.match(/\w/g).sort(sortMethod);
+
+  if (cleanA.length !== cleanB.length) {
+    return false;
+  }
+
+  //.every would work here also
+  return (
+    cleanA
+      .reduce((s, c) => s + c.toLowerCase(), "")
+      .localeCompare(cleanB.reduce((s, c) => s + c.toLowerCase(), "")) === 0
+  );
+}
+
+//Stephens sort solution
+function cleanString(str) {
+  return str.replace(/[^\w]/g, "").toLowerCase().split("").sort().join("");
+}
+
+function anagrams(stringA, stringB) {
+  return cleanString(stringA) === cleanString(stringB);
+}
+
+anagrams("Whoa! Hi!", " Hi! Whoa!");
 
 module.exports = anagrams;
--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,51 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+// mine
+function chunk_ryan(array, size) {
+  const chunks = [];
+
+  for (let a = 0; a < array.length; a = a + size) {
+    const inner = [];
+    for (let s = 0; s < size; s++) {
+      const element = array[a + s];
+
+      if (element === undefined) {
+        break;
+      }
+
+      inner.push(element);
+    }
+    chunks.push(inner);
+  }
+  return chunks;
+}
+
+// Stephen's
+// function chunk(array, size) {
+//   const chunked = [];
+
+//   for (const element of array) {
+//     const last = chunked[chunked.length - 1];
+
+//     if (!last || last.length === size) {
+//       chunked.push([element]);
+//     } else {
+//       last.push(element);
+//     }
+//   }
+
+//   return chunked;
+// }
+
+//Stephen alternate
+function chunk(array, size) {
+  const chunked = [];
+  for (let index = 0; index < array.length; index += size) {
+    chunked.push(array.slice(index, index + size));
+  }
+
+  return chunked;
+}
 
 module.exports = chunk;

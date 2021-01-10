f2d34cf5627207d1aea36cec8d0957ee5770cc74
Ryan Pfister <puhfister@gmail.com>
2021-01-10 08:10:20 -0500

    steps and capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,47 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize_ryan(str) {
+  return str
+    .split(" ")
+    .map((word) => word[0].toUpperCase() + word.slice(1))
+    .join(" ");
+}
+
+//stephens first
+function capitalize_first(str) {
+  const words = [];
+
+  for (const word of str.split(" ")) {
+    words.push(word[0].toUpperCase() + word.slice(1));
+  }
+
+  return words.join(" ");
+}
+
+//stephens second
+function capitalize_second(str) {
+  let result = str[0].toUpperCase();
+
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] === " ") {
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i];
+    }
+  }
+
+  return result;
+}
+
+//ryan second
+function capitalize(str) {
+  return str
+    .split(" ")
+    .reduce(
+      (s, w) => s + ((!!s && " ") || "") + w[0].toUpperCase() + w.slice(1),
+      ""
+    );
+}
 
 module.exports = capitalize;
--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,70 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+//start Ryan recursive
+function step_recursion(now, goal) {
+  if (now > goal) {
+    return;
+  }
+
+  const pound = Array(now).fill("#");
+  const space = Array(goal - now).fill(" ");
+  console.log(pound.join("") + space.join(""));
+  return step_recursion(now + 1, goal);
+}
+
+function steps_ryan(n) {
+  step_recursion(1, n);
+}
+//end Ryan recursive
+
+// Stephen iterative solution
+function steps_stephen_iterative(n) {
+  for (let row = 0; row < n; row++) {
+    let stair = "";
+    for (let column = 0; column < n; column++) {
+      if (column <= row) {
+        stair += "#";
+      } else {
+        stair += " ";
+      }
+    }
+    console.log(stair);
+  }
+}
+
+//ryans iterative solution (with help)
+function steps_ryan_iterative(n) {
+  for (let row = 0; row < n; row++) {
+    const stair = Array(n);
+    for (let column = 0; column < n; column++) {
+      if (column <= row) {
+        stair.push("#");
+      } else {
+        stair.push(" ");
+      }
+    }
+    console.log(stair.join(""));
+  }
+}
+
+//stephens recursive solution
+// row = how many of these have we printed
+function steps(n, row = 0, stair = "") {
+  if (n === row) {
+    return;
+  }
+
+  //have we built out the string to the correct size?
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  //do we add on a pound or a space?
+  const add = stair.length <= row ? "#" : " ";
+
+  steps(n, row, stair + add);
+}
 
 module.exports = steps;
